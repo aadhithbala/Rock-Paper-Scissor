e7e17e2d0e09a216047ab6a54ce38d9b31331ede
aadhithbala <aadhi66613@gmail.com>
2023-06-27 13:56:44 +0530

    Write fn to capture winner of 5 rounds

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 'use strict';
 
+// Global Variable to capture the scores
 let playerScore = 0;
 let computerScore = 0;
 
+//Function to get computers choice - Rock, Paper or Scissor
 const getComputerChoice = function () {
   const computerChoice = Math.trunc(Math.random() * 3);
 
@@ -15,6 +17,7 @@ const getComputerChoice = function () {
   }
 };
 
+//Function to sanitize the user input to Capitalize the first letter
 const capitalize = function (playerChoice) {
   const playerChoiceLowerCase = playerChoice.toLowerCase();
   return (
@@ -23,6 +26,7 @@ const capitalize = function (playerChoice) {
   );
 };
 
+//Function to play one round of game
 const playRound = function (playerChoice, computerChoice) {
   if (
     (playerChoice === 'Rock' && computerChoice === 'Scissor') ||
@@ -39,14 +43,32 @@ const playRound = function (playerChoice, computerChoice) {
   }
 };
 
+// Function to check the winner of 5 rounds
+const winner = function (playerScore, computerScore) {
+  if (playerScore > computerScore) {
+    console.log(
+      `You Win! Your Score: ${playerScore}, Computer Score: ${computerScore}`
+    );
+  } else if (playerScore < computerScore) {
+    console.log(
+      `You Lose! Your Score: ${playerScore}, Computer Score: ${computerScore}`
+    );
+  } else
+    console.log(
+      `Tie! Your Score: ${playerScore}, Computer Score: ${computerScore}`
+    );
+};
+
 const game = function () {
   for (let i = 1; i < 6; i++) {
     const computerSelection = getComputerChoice(); //Invoking Function for computer choice
 
     const playerSelection = capitalize(prompt('Enter your choice:')); // Prompting the user for users choice
 
-    playRound(playerSelection, computerSelection);
+    playRound(playerSelection, computerSelection); //Invoking function to play a round of game
   }
+
+  winner(playerScore, computerScore); //Invoking function to check winner of 5 rounds
 };
 
 game();
